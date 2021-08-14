1d947d12c5691452b6122cf6a7e3b5358d7c6f64
PrasadReddy23 <prasadappala8@gmail.com>
2021-08-14 18:30:38 +1000

    fianl101

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,13 +126,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = [
- BASE_DIR / "static", '/projects/static/',
-]
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
@@ -146,3 +145,4 @@ django_heroku.settings(locals())
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
