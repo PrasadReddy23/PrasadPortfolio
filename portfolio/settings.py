ba34d1a780218f0bd79b9ef2fcf0bad90c47caed
PrasadReddy23 <prasadappala8@gmail.com>
2021-08-14 18:10:21 +1000

    Initial Deployment 2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,9 +129,10 @@ PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'projects/static'),
-)
+STATICFILES_DIRS = [
+ BASE_DIR / "static", '/projects/static/',
+]
+
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
